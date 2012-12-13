f785dc894685e0bdbc168b065ded49563569a2d0
Lee Irvine <loofy2@gmail.com>
2012-12-12 23:35:25 -0500

    Initial Commit

---
+++ ActionSheet/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ActionSheet
+//
+//  Created by Lee Irvine on 12/12/12.
+//  Copyright (c) 2012 kezzi.co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ActionSheet/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ActionSheet
+//
+//  Created by Lee Irvine on 12/12/12.
+//  Copyright (c) 2012 kezzi.co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
